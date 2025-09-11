03bb1ebea3ab2f3e53d9cd2d9d97e18e7775dec1
1004yeeun <joycejeoung@ucla.edu>
2025-09-10 18:39:39 -0700

    Add skills and tech bubbles to project components

--- src/app/components/Project.jsx
+++ src/app/components/Project.jsx
@@ -1,13 +1,32 @@
 import Image from 'next/image';
 import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa';
 
-export default function Project({ name, description, image }) {
+export default function Project({ name, description, image, tech = [], skill = [] }) {
     return (
         <div className="rounded-2xl shadow-md p-4 bg-white hover:shadow-xl transition">
             <div className="flex flex-col items-center md:flex-row md:items-center gap-3">
                 <div className="flex-1">
-                    <h2 className="text-xl font-semibold">{name}</h2>
-                    <h2 className="text-black">{description}</h2>
+                    <h2 className="text-xl font-semibold mb-1 text-black">{name}</h2>
+                    <p className="text-black">{description}</p>
+
+                    <div className="flex flex-wrap gap-1 mb-1 pt-4">
+                        {tech.map((t, i) => (
+                            <span
+                                key={i}
+                                className="px-2 py-1 text-sm bg-[#e7e6e4] text-[#353433bd] rounded-full"
+                            >
+                                {t}
+                            </span>
+                        ))}
+                        {skill.map((s, j) => (
+                            <span
+                                key={j}
+                                className="px-2 py-1 text-sm bg-[#cfcdc8] text-[#5a5956bd] rounded-full"
+                            >
+                                {s}
+                            </span>
+                        ))}
+                    </div>
                 </div>
                 <div className="md:ml-2 shrink-0">
                     <div className="relative w-50 h-50 md:w-60 md:h-60 rounded-xl overflow-hidden">
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -9,30 +9,40 @@ const ProjectSection = () => {
                 name="FoodieFam"
                 description="FoodieFam is a web app built atop a MERN stack that aims to connect people to share a meal with. Have you ever wanted to try multiple dishes from a restaurant while on a solo mission? Use Foodie Fam to find others to share a meal family style!"
                 image="/images/foodie-fam-image.png"
+                tech={["React", "Node.js", "Express", "MongoDB Atlas (NoSQL)", "Git/GitHub"]}
+                skill={["Full-Stack", "Backend Dev", "Authentication/Secure Login", "Rest API", "Leadership", "Team Collaboration", "Communication", "Version Control"]}
             />
             
             <Project
                 name="Marble Madness"
                 description="Marble Madness is a 2-dimensional maze-based video game where you battle enemies and collect power-ups along your journey in guarding crystals to the finish line within the shortest amount of time. This C++ game implements inheritance, polymorphism, and memory management to optimize performance and enhance system architecture with object-oriented programming."
                 image="/images/marble-madness-image.png"
+                tech={["C++"]}
+                skill={["Object-Oriented Programming", "Inheritance", "Polymorphism", "Memory Management", "Debugging", "Optimization"]}
             />
 
             <Project
                 name="Slice"
                 description="Slice is a browser-based video editing tool built to efficiently crop and organize sports footage with precision and speed. Slice allows users to extract specific highlights from multi-hour tennis matches and group them by shot type for AI-driven analysis. "
                 image="/images/slice-image.png"
+                tech={["Firebase", "PyTorch", "TypeScript", "FFmpeg.js", "React", "Node.js"]}
+                skill={["Browser-based Video Processing", "Frontend Performance Optimization", "In-browser File Handling"]}
             />
 
             <Project
                 name="Phichi.com"
                 description="PhiChi.com is a digital home base for UCLA's pre-med students seeking mentorship, leadership, and community. When the Phi Chi Professional Pre-Medical Society first launched its chapter, there was no digital infrastructure - no online presence, no member system, no place for students to find support. I took initiative to build the organization's website from the ground up, transforming a blank canvas into a responsive, content-rich hub that continues to grow with the chapter."
                 image="/images/phichi-image.png"
+                tech={["HTML/CSS", "WordPress"]}
+                skill={["UI/UX", "Web Dev", "Content Management Systems", "User-centered Design", "Technical Initiative"]}
             />
 
             <Project
                 name="Personal Portfolio"
                 description="Joycejeoung.com is a personal portfolio website that I built and deployed to showcase my projects, experiences, and technical skills. The site serves as an evolving hub for my software engineering journey and reflects my growth as a developer."
                 image="/images/personal-portfolio-image.png"
+                tech={["React", "Next.js", "Tailwind CSS"]}
+                skill={["UI/UX", "Personal Branding", "Frontend Dev", "Reusable Component Design", "Organization"]}
             />
         </div>
     );

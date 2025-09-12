a7b944540623aca6e95f8c5478efe83ce83b6613
1004yeeun <joycejeoung@ucla.edu>
2025-09-12 13:14:23 -0700

    Add click sound to footer buttons

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,16 +1,19 @@
+"use client"
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import useClickSound from '../hooks/useClickSound'; 
 
 const Footer = () => {
+    const playClick = useClickSound();
     return (
         <div className="flex justify-center gap-6 mb-8 text-3xl pt-3 pb-10">
             <a href="https://github.com/1004yeeun" target="_blank" className="text-black hover:text-[#93908c] transition">
-                <FaGithub />
+                <FaGithub onClick={ () => {playClick();} }/>
             </a>
             <a href="https://www.linkedin.com/in/joycejeoung" target="_blank" className="text-black hover:text-[#93908c] transition">
-                <FaLinkedin />
+                <FaLinkedin onClick={ () => {playClick();} }/>
             </a>
             <a href="mailto:joycejeoung@ucla.edu" className="text-black hover:text-[#93908c] transition">
-                <FaEnvelope />
+                <FaEnvelope onClick={ () => {playClick();} }/>
             </a>
         </div>
     );
